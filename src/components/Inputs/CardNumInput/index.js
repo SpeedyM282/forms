fe47918621ebb4df51533abbd6bfce5bf7648f8f
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-03 02:48:31 +0500

    refactor CardNumInput component code

--- src/components/Inputs/CardNumInput/index.js
+++ src/components/Inputs/CardNumInput/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
 import './style.scss';
 
-function CardNumInput({ label, type }) {
-  const [value, setValue] = useState('');
-
+function CardNumInput({ value, onChange, field }) {
   const handleChange = event => {
     const result = event.target.value.replace(/\D/g, '').replace(/(.{4})/g, '$1 ').trim();
-    setValue(result);
+    onChange(field, result)
   };
 
   return (
     <>
       <label className='form__label' >
-        {label}:
+        Card Number:
         <input
           className='form__input--card-num'
           maxLength={19}
-          type={type}
+          type='text'
           name="card-num"
           value={value}
           onChange={handleChange}
